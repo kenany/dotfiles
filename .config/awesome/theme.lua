5cef11ef52b6fa9d6ced1865b72307b26e8ac2c1
Kenan Yildirim <kenan@kenany.me>
2013-12-08 14:46:50 -0500

    Add awesomewm theme

---
+++ .config/awesome/theme.lua
@@ -0,0 +1,97 @@
+theme = {}
+
+theme.font          = "lemon 7"
+
+theme.bg_normal     = "#20202f"
+theme.bg_focus      = "#c97c7c"
+theme.bg_urgent     = "#20202f"
+theme.bg_minimize   = "#20202f"
+theme.bg_systray    = theme.bg_normal
+
+theme.fg_normal     = "#b4b4b4"
+theme.fg_focus      = "#eeeeee"
+theme.fg_urgent     = "#646464"
+theme.fg_minimize   = "#eeeeee"
+theme.fg_systray    = "#b4b4b4"
+
+theme.border_width  = 2
+theme.border_normal = "#646464"
+theme.border_focus  = "#b4b4b4"
+theme.border_marked = "#646464"
+
+-- Display the taglist squares
+theme.taglist_squares_sel   = "/usr/share/awesome/themes/default/taglist/squarefw.png"
+theme.taglist_squares_unsel = "/usr/share/awesome/themes/default/taglist/squarew.png"
+
+-- Variables set for theming the menu
+theme.menu_submenu_icon = "/usr/share/awesome/themes/default/submenu.png"
+theme.menu_height = 15
+theme.menu_width  = 100
+
+-- You can add as many variables as you wish and access them by using
+-- `beautiful.variable` in your `rc.lua`.
+
+-- Set the spacing between tiled windows to 43px.
+theme.useless_gap_width = "43"
+
+-- Set the height of the lowest window of the second column to 72px; setting it
+-- to zero makes all the slave windows the same height.
+theme.lower_window_height = "72"
+
+-- Set your vertical resolution.
+theme.vertical_resolution = "1080"
+
+-- Set the extra vertical spacing to 8px.
+theme.vertical_border = "3"
+
+-- Set the extra outermost spacing to 18px.
+theme.outer_padding = "0"
+
+
+-- Define the image to load
+theme.titlebar_close_button_normal = "/usr/share/awesome/themes/default/titlebar/close_normal.png"
+theme.titlebar_close_button_focus  = "/usr/share/awesome/themes/default/titlebar/close_focus.png"
+
+theme.titlebar_ontop_button_normal_inactive = "/usr/share/awesome/themes/default/titlebar/ontop_normal_inactive.png"
+theme.titlebar_ontop_button_focus_inactive  = "/usr/share/awesome/themes/default/titlebar/ontop_focus_inactive.png"
+theme.titlebar_ontop_button_normal_active = "/usr/share/awesome/themes/default/titlebar/ontop_normal_active.png"
+theme.titlebar_ontop_button_focus_active  = "/usr/share/awesome/themes/default/titlebar/ontop_focus_active.png"
+
+theme.titlebar_sticky_button_normal_inactive = "/usr/share/awesome/themes/default/titlebar/sticky_normal_inactive.png"
+theme.titlebar_sticky_button_focus_inactive  = "/usr/share/awesome/themes/default/titlebar/sticky_focus_inactive.png"
+theme.titlebar_sticky_button_normal_active = "/usr/share/awesome/themes/default/titlebar/sticky_normal_active.png"
+theme.titlebar_sticky_button_focus_active  = "/usr/share/awesome/themes/default/titlebar/sticky_focus_active.png"
+
+theme.titlebar_floating_button_normal_inactive = "/usr/share/awesome/themes/default/titlebar/floating_normal_inactive.png"
+theme.titlebar_floating_button_focus_inactive  = "/usr/share/awesome/themes/default/titlebar/floating_focus_inactive.png"
+theme.titlebar_floating_button_normal_active = "/usr/share/awesome/themes/default/titlebar/floating_normal_active.png"
+theme.titlebar_floating_button_focus_active  = "/usr/share/awesome/themes/default/titlebar/floating_focus_active.png"
+
+theme.titlebar_maximized_button_normal_inactive = "/usr/share/awesome/themes/default/titlebar/maximized_normal_inactive.png"
+theme.titlebar_maximized_button_focus_inactive  = "/usr/share/awesome/themes/default/titlebar/maximized_focus_inactive.png"
+theme.titlebar_maximized_button_normal_active = "/usr/share/awesome/themes/default/titlebar/maximized_normal_active.png"
+theme.titlebar_maximized_button_focus_active  = "/usr/share/awesome/themes/default/titlebar/maximized_focus_active.png"
+
+theme.wallpaper = "/home/kenan/Pictures/Wallpapers/2849325.jpg"
+
+-- You can use your own layout icons like this:
+theme.layout_fairh = "/usr/share/awesome/themes/default/layouts/fairhw.png"
+theme.layout_fairv = "/usr/share/awesome/themes/default/layouts/fairvw.png"
+theme.layout_floating  = "/usr/share/awesome/themes/default/layouts/floatingw.png"
+theme.layout_magnifier = "/usr/share/awesome/themes/default/layouts/magnifierw.png"
+theme.layout_max = "/usr/share/awesome/themes/default/layouts/maxw.png"
+theme.layout_fullscreen = "/usr/share/awesome/themes/default/layouts/fullscreenw.png"
+theme.layout_tilebottom = "/usr/share/awesome/themes/default/layouts/tilebottomw.png"
+theme.layout_tileleft   = "/usr/share/awesome/themes/default/layouts/tileleftw.png"
+theme.layout_tile = "/usr/share/awesome/themes/default/layouts/tilew.png"
+theme.layout_tiletop = "/usr/share/awesome/themes/default/layouts/tiletopw.png"
+theme.layout_spiral  = "/usr/share/awesome/themes/default/layouts/spiralw.png"
+theme.layout_dwindle = "/usr/share/awesome/themes/default/layouts/dwindlew.png"
+
+theme.awesome_icon = "/usr/share/awesome/icons/awesome16.png"
+
+-- Define the icon theme for application icons. If not set then the icons from
+-- `/usr/share/icons` and `/usr/share/icons/hicolor` will be used.
+theme.icon_theme = nil
+
+return theme
