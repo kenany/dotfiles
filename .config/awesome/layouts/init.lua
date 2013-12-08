b97e5050c8a27551fd67998578d19d1e7514df8a
Kenan Yildirim <kenan@kenany.me>
2013-12-08 13:39:34 -0500

    Add awesomewm layouts

---
+++ .config/awesome/layouts/init.lua
@@ -0,0 +1,7 @@
+local layouts = { _NAME = "layouts" }
+return setmetatable(layouts, {
+  __index = function(table, key)
+    local module = rawget(table, key)
+    return module or require(table._NAME .. "." .. key)
+  end
+})
