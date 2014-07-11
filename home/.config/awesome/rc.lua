3b4364d85aa2e5e5acf2ba6dad20c014e0d2b25a
Kenan Yildirim <kenan@kenany.me>
2014-07-11 17:25:35 -0400

    Add bottom system bar

--- home/.config/awesome/rc.lua
+++ home/.config/awesome/rc.lua
@@ -160,6 +160,11 @@ menubar.utils.terminal = terminal
 
 -- {{{ Wibox
 
+
+-- OS.
+sys = wibox.widget.textbox()
+vicious.register(sys, vicious.widgets.os, "$1 $2")
+
 -- Create a textclock widget.
 mytextclock = awful.widget.textclock("   %I:%M %p - %Y-%m-%d (%A) ")
 daze.widgets.calendar.register(mytextclock)
@@ -179,7 +184,7 @@ mytaglist.buttons = awful.util.table.join(
 )
 mytasklist = {}
 mytasklist.buttons = awful.util.table.join(
-  awful.button({}, 1, function (c)
+  awful.button({}, 1, function(c)
     if c == client.focus then
       c.minimized = true
     else
@@ -195,7 +200,8 @@ mytasklist.buttons = awful.util.table.join(
       c:raise()
     end
   end),
-  awful.button({}, 3, function ()
+
+  awful.button({}, 3, function()
     if instance then
       instance:hide()
       instance = nil
@@ -203,11 +209,13 @@ mytasklist.buttons = awful.util.table.join(
       instance = awful.menu.clients({ width=250 })
     end
   end),
-  awful.button({}, 4, function ()
+
+  awful.button({}, 4, function()
     awful.client.focus.byidx(1)
     if client.focus then client.focus:raise() end
   end),
-  awful.button({}, 5, function ()
+
+  awful.button({}, 5, function()
     awful.client.focus.byidx(-1)
     if client.focus then client.focus:raise() end
   end)
@@ -222,10 +230,10 @@ for s = 1, screen.count() do
   -- layout we're using. We need one layoutbox per screen.
   mylayoutbox[s] = awful.widget.layoutbox(s)
   mylayoutbox[s]:buttons(awful.util.table.join(
-    awful.button({}, 1, function () awful.layout.inc(layouts, 1) end),
-    awful.button({}, 3, function () awful.layout.inc(layouts, -1) end),
-    awful.button({}, 4, function () awful.layout.inc(layouts, 1) end),
-    awful.button({}, 5, function () awful.layout.inc(layouts, -1) end)
+    awful.button({}, 1, function() awful.layout.inc(layouts, 1) end),
+    awful.button({}, 3, function() awful.layout.inc(layouts, -1) end),
+    awful.button({}, 4, function() awful.layout.inc(layouts, 1) end),
+    awful.button({}, 5, function() awful.layout.inc(layouts, -1) end)
   ))
 
   -- Create a taglist widget.
@@ -234,7 +242,7 @@ for s = 1, screen.count() do
   -- Create a tasklist widget.
   mytasklist[s] = awful.widget.tasklist(s, awful.widget.tasklist.filter.currenttags, mytasklist.buttons)
 
-  -- Create the wibox.
+  -- Create the top wibox.
   mywibox[s] = awful.wibox({position = "top", screen = s})
 
   -- Widgets that are aligned to the left.
@@ -254,6 +262,19 @@ for s = 1, screen.count() do
   layout:set_left(left_layout)
   layout:set_middle(mytasklist[s])
   layout:set_right(right_layout)
+
+  mywibox[s]:set_widget(layout)
+
+  -- Create the bottom wibox
+  mywibox[s] = awful.wibox({position = "bottom", screen = s})
+
+  -- Widgets that are aligned to the left.
+  local left_layout = wibox.layout.fixed.horizontal()
+  left_layout:add(sys)
+
+  -- Now bring it all together.
+  local layout = wibox.layout.align.horizontal()
+  layout:set_left(left_layout)
 
   mywibox[s]:set_widget(layout)
 end
