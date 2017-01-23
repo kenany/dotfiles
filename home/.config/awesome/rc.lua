0217258017c170c321039d53ff3011d8f9770d29
Kenan Yildirim <kenan@kenany.me>
2017-01-23 12:51:55 -0500

    awesome: update client keys

--- home/.config/awesome/rc.lua
+++ home/.config/awesome/rc.lua
@@ -403,23 +403,42 @@ local globalkeys = awful.util.table.join(
 )
 
 local clientkeys = awful.util.table.join(
-  awful.key({modkey}, "f", function(c) c.fullscreen = not c.fullscreen end),
-  awful.key({modkey}, "c", function(c) c:kill() end),
-  awful.key({modkey, "Control"}, "space",  awful.client.floating.toggle),
+  awful.key({modkey}, "f", function (c)
+      c.fullscreen = not c.fullscreen
+      c:raise()
+    end,
+    {description = "toggle fullscreen", group = "client"}
+  ),
+  awful.key({modkey}, "c", function (c) c:kill() end,
+    {description = "close", group = "client"}
+  ),
+  awful.key({modkey, "Control"}, "space", awful.client.floating.toggle,
+    {description = "toggle floating", group = "client"}
+  ),
   awful.key({modkey, "Control"}, "Return", function(c)
-    c:swap(awful.client.getmaster())
-  end),
-  awful.key({modkey}, "o", awful.client.movetoscreen),
-  awful.key({modkey}, "t", function(c) c.ontop = not c.ontop end),
+      c:swap(awful.client.getmaster())
+    end,
+    {description = "move to master", group = "client"}
+  ),
+  awful.key({modkey}, "o", function(c) c:move_to_screen() end,
+    {description = "move to screen", group = "client"}
+  ),
+  awful.key({modkey}, "t", function(c) c.ontop = not c.ontop end,
+    {description = "toggle keep on top", group = "client"}
+  ),
   awful.key({modkey}, "n", function (c)
       -- The client currently has the input focus, so it cannot be minimized,
       -- since minimized clients can't have the focus.
       c.minimized = true
-  end),
-  awful.key({modkey}, "m", function(c)
-    c.maximized_horizontal = not c.maximized_horizontal
-    c.maximized_vertical = not c.maximized_vertical
-  end)
+    end,
+    {description = "minimize", group = "client"}
+  ),
+  awful.key({modkey}, "m", function (c)
+      c.maximized = not c.maximized
+      c:raise()
+    end,
+    {description = "maximize", group = "client"}
+  )
 )
 
 -- Bind all key numbers to tags.
