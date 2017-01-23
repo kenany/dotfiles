114f9474620013cf229335721cd6a0d49535621e
Kenan Yildirim <kenan@kenany.me>
2017-01-23 11:22:27 -0500

    awesome: update wallpaper code
    This comes from the new `rc.lua` of awesome v4.

--- home/.config/awesome/rc.lua
+++ home/.config/awesome/rc.lua
@@ -76,11 +76,20 @@ local layouts = {
 beautiful.init("/home/kenan/.config/awesome/theme.lua")
 
 -- Wallpaper
-if beautiful.wallpaper then
-  for s = 1, screen.count() do
-    gears.wallpaper.maximized(beautiful.wallpaper, s, true)
+local function set_wallpaper(s)
+  if beautiful.wallpaper then
+    local wallpaper = beautiful.wallpaper
+
+    -- If wallpaper is a function, call it with the screen
+    if type(wallpaper) == "function" then
+      wallpaper = wallpaper(s)
+    end
+    gears.wallpaper.maximized(wallpaper, s, true)
   end
 end
+
+-- Re-set wallpaper when a screen's geometry changes (e.g. different resolution)
+screen.connect_signal("property::geometry", set_wallpaper)
 
 -- Define a tag table which hold all screen tags.
 local tags = {
