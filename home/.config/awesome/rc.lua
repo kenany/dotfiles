c370cd68c831bf5f02e051eee38f8d213da76dfc
Kenan Yildirim <kenan@kenany.me>
2017-01-23 12:38:31 -0500

    awesome: fix indent

--- home/.config/awesome/rc.lua
+++ home/.config/awesome/rc.lua
@@ -500,12 +500,12 @@ awful.rules.rules = {
 
 -- Signal function to execute when a new client appears
 client.connect_signal("manage", function (c)
-    if awesome.startup and
-      not c.size_hints.user_position
-      and not c.size_hints.program_position then
-        -- Prevent clients from being unreachable after screen count changes.
-        awful.placement.no_offscreen(c)
-    end
+  if awesome.startup
+    and not c.size_hints.user_position
+    and not c.size_hints.program_position then
+    -- Prevent clients from being unreachable after screen count changes.
+    awful.placement.no_offscreen(c)
+  end
 end)
 
 -- Add a titlebar if titlebars_enabled is set to true in the rules.
